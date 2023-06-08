2d9b4c6c843853930f09a23634cebdc1abd8abd9
romainniamor <romainnavoret@gmail.com>
2023-06-08 22:45:45 +0200

    structure ok

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,69 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" nav app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">Planning</v-list-item-title>
+          <v-list-item-subtitle>MENU</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <v-list nav color="info">
+        <template v-for="item in items">
+          <v-list-item
+            :prepend-icon="item.icon"
+            :title="item.title"
+            :value="item.value"
+            :to="item.to"
+          ></v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title></v-toolbar-title>
+    </v-app-bar>
 
-  <RouterView />
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+
+    <v-footer class="bg-grey-lighten-4 text-center d-flex flex-column" app>
+      <div>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 bg-info"
+          :icon="icon"
+          variant="text"
+        ></v-btn>
+      </div>
+    </v-footer>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      items: [
+        {
+          icon: 'mdi-view-dashboard',
+          title: 'Dashboard',
+          value: 'dashboard',
+          to: '/'
+        },
+        {
+          icon: 'mdi-calendar',
+          title: 'Planning',
+          value: 'planning',
+          to: '/planning'
+        }
+        // plus de selection
+      ]
+    }
   }
 }
-</style>
+</script>
---
+++ src/views/planning.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>planning</h1>
+</template>
